a83b336877d76e53f7ccf825f79f9749d2e2e732
Haden Moore <hadens.moore@gmail.com>
2020-02-28 13:54:57 -0600

    Update

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,129 +3,90 @@
 import sys
 
 # Hardcoding variables for branch table
-# opcodes
-LDI = 0b10000010
-PRN = 0b01000111
-HLT = 0b00000001
+LDI = 0b10000010 # LDI R0, 8
+PRN = 0b01000111 # PRN R0
+HLT = 0b00000001 # HLT
 ADD = 0b10100000
 MUL = 0b10100010
 POP = 0b01000110
 PUSH = 0b01000101
 CALL = 0b01010000
 RET = 0b00010001
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
+SP = 7  # Stack Pointer (SP) Register 7
 
-SP = 7  # Stack pointer is Register 7
 
-class CPU:
-    """Main CPU class."""
+class CPU: 
+    """Main CPU Class."""
 
     def __init__(self):
-        """Construct a new CPU."""
-        self.pc = 0
-        self.reg = [0] * 8
-        self.ram = [0] * 256
-        self.bt = {
-            LDI: self.op_ldi,
-            PRN: self.op_prn,
-            HLT: self.op_hlt,
-            ADD: self.op_add,
-            MUL: self.op_mul,
-            POP: self.op_pop,
-            PUSH: self.op_push,
-            CALL: self.op_call,
-            RET: self.op_ret
-        }
+        """Construct a New CPU.""" 
+         # 256 Bytes of Mem with 8 GPRs
+        self.ram = [0] * 256 # 256 Bytes of Mem for Instructions
+        self.reg = [0] * 8 # 8 GPRs (General-Purpose Registers)
+        self.pc = 0 # Helps Distinguish b/w Operands and Instructions
+        self.reg[7] = 255
+        self.sp = self.reg[7]
+        self.flag = None
 
+        def ram_read(self, MAR):
+            return self.ram[MAR]
 
-    def op_ldi(self, operand_a, operand_b):
-        self.reg[operand_a] = operand_b
-
-    def op_prn(self, operand_a, operand_b):
-        print(self.reg[operand_a])
-
-    def op_hlt(self, operand_a, operand_b):
-        sys.exit(0)
-
-    def op_add(self, operand_a, operand_b):
-        self.alu("ADD", operand_a, operand_b)
-
-    def op_mul(self, operand_a, operand_b):
-        self.alu("MUL", operand_a, operand_b)
-
-    def op_pop(self, operand_a, operand_b):
-        self.reg[operand_a] = self.pop_val()
-
-    def op_push(self, operand_a, operand_b):
-        self.push_val(self.reg[operand_a])
-
-    def op_call(self, operand_a, operand_b):
-        self.push_val(self.pc + 2)
-        self.pc = self.reg[operand_a]
-
-    def op_ret(self, operand_a, operand_b):
-        self.pc = self.pop_val()
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self, filename):
-        """Load a program into memory."""
-        try:
-            address = 0
-            # Open the file
-            with open(sys.argv[1]) as f:
-                # Read all the lines
-                for line in f:
-                    # Parse out the comments
-                    comment_split = line.strip().split("#")
-                    # Cast number strings to ints
-                    value = comment_split[0].strip()
-                    # Ignore blank lines
-                    if value == "":
-                        continue
-                    instruction = int(value, 2)
-                    # Populate a memory array
-                    self.ram[address] = instruction
+        """Load a Program into Memory."""
+
+        address = 0
+
+        with open(filename) as file:
+            for line in file:
+                command_split = line.split('#')
+                instruction = command_split[0]
+
+                if instruction == "":
+                    continue
+
+                bit_One = instruction[0]
+
+                if bit_One == '0' or bit_One == '1':
+                    self.ram[address] = int(instruction[:8], 2) # convert instructions to binary
                     address += 1
 
-        except FileNotFoundError:
-            print("File not found")
-            sys.exit(2)
-
-    def ram_read(self, mar):
-        mdr = self.ram[mar]
-        return mdr
-
-    def ram_write(self, mdr, mar):
-        self.ram[mar] = mdr
-
-    def push_val(self, val):
-        self.reg[SP] -= 1
-        self.ram_write(val, self.reg[SP])
-
-    def pop_val(self):
-        val = self.ram_read(self.reg[SP])
-        self.reg[SP] += 1
-        return val
-
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+        """ALU operations. Arithetic logic unit"""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
-        # elif op == "SUB": etc
+
+        elif op == "CMP":
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.flag = 0b00000001
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.flag = 0b00000010
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                self.flag = 0b00000100
+
         else:
             raise Exception("Unsupported ALU operation")
 
     def trace(self):
         """
-        Handy function to print out the CPU state. You might want to call this
+        A Handy Function to Print out the CPU State. You might want to call this
         from run() if you need help debugging.
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            # self.fl,
-            # self.ie,
+            #self.fl,
+            #self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -138,15 +99,65 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        while True:
-            instruction = self.ram[self.pc]
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
-            instruction_length = (instruction >> 6) + 1
-            instruction_set_pc = (instruction >> 4) == 1
-            if instruction in self.bt:
-                self.bt[instruction](operand_a, operand_b)
-            # Check if opcode sets the pc
-            # If not, increment pc by instruction_length
-            if instruction_set_pc != 1:
-                self.pc += instruction_length
+
+        running = True
+
+        while running:
+            command = self.ram_read(self.pc) # get instructions
+
+            op_A = self.ram_read(self.pc + 1) # Reg Location
+            op_B = self.ram_read(self.pc + 2) # Value
+
+            op_Size = int(command) >> 6 # Get Operand Size by Shifting Six
+            self.pc += (1 + op_Size)
+
+            if command == LDI:
+                self.reg[op_A] = op_B
+                # self.pc += 3
+            
+            elif command == PRN:
+                op_A = self.ram[self.pc + 1]
+                print(self.reg[op_A])
+                # self.pc += 2
+
+            elif command == MUL:
+                self.alu("MUL", op_A, op_B)
+                # self.pc += 3
+
+            elif command == HLT:
+                running = False
+
+            elif command == PUSH:
+                self.sp = (self.sp % 257) - 1
+                self.ram[self.sp] = self.reg[op_A]
+                # self.pc += 2
+            
+            elif command == POP:
+                self.reg[op_A] = self.ram[self.sp]
+                self.sp = (self.sp % 257) + 1
+                # self.pc += 2
+            
+            elif command == CALL:
+                self.sp -= 1
+                self.ram[self.sp] = self.pc + 2
+
+            elif command == RET:
+                self.pc = self.ram[self.sp]
+            
+            # Compare
+            elif command == CMP:
+                self.alu("CMP", op_A, op_B)
+
+            # Jump to PC Address
+            elif command == JMP:
+                self.pc = self.reg[op_A]
+            
+            # Jump if Equal
+            elif command == JEQ:
+                if self.flag == 0b00000001:
+                    self.pc = self.reg[op_A]
+
+            # Jump if Not Equal
+            elif command == JNE:
+                if self.flag != 0b00000001:
+                    self.pc = self.reg[op_A]
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python3
-
-"""Main."""
-
 import sys
-import cpu
 from cpu import *
 
-cpu = CPU()
-
-if len(sys.argv) != 2:
-    print("ERROR: must have file name")
-    sys.exit(1)
-
-cpu.load(sys.argv[1])
-cpu.run()
+if len(sys.argv) == 2:
+    cpu = CPU()
+    cpu.load(sys.argv[1]) # pass the filename to load
+    cpu.run()
+else:
+    print("error: please provide filename to execute function")
