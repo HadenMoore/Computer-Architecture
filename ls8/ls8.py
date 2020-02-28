474e77a983e9afbaf4a695674d60aef00b16060d
Haden Moore <hadens.moore@gmail.com>
2020-02-28 14:22:49 -0600

    Update

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -3,7 +3,7 @@ from cpu import *
 
 if len(sys.argv) == 2:
     cpu = CPU()
-    cpu.load(sys.argv[1]) # pass the filename to load
+    cpu.load(sys.argv[1]) # Pass File Name to Load
     cpu.run()
 else:
-    print("error: please provide filename to execute function")
+    print("Error!: Provide a File Name to Execute this Function.")
